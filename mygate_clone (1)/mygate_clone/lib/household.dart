0b718868898ca5982a5abb663489d0c8935d2d24
Madhusudan23082000 <73375985+Madhusudan23082000@users.noreply.github.com>
2021-07-05 13:39:08 +0530

    Add files via upload

---
+++ mygate_clone (1)/mygate_clone/lib/household.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+
+class Household extends StatefulWidget {
+  @override
+  _HouseholdState createState() => _HouseholdState();
+}
+
+class _HouseholdState extends State<Household> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Hi, name',
+            style: TextStyle(
+              gradient: LinearGradient(colors: [Colors.pink, Colors.orange]),
+            ),
+          ),
+        ),//appbar
+        backgroundColor: Colors.white,
+        body: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Icon(Icons.menu, color: Colors.white,size: 52.0,),
+
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: Text(
+                    "Name (ME) \nShare my Address",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold
+                    ),
+                    textAlign: TextAlign.start,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Center(
+                    child: Wrap(
+                      spacing:20,
+                      runSpacing: 20.0,
+                      children: <Widget>[
+                        SizedBox(
+                          width:160.0,
+                          height: 160.0,
+                          child: Card(
+                            // image
+                            color: Color.white,
+                            elevation: 2.0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0)
+                            ),
+                            child:Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    children: <Widget>[
+                                      // Image icons
+                                      SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      SizedBox(
+                                        height: 5.0,
+                                      ),
+                                    ],
+                                  ),
+                                )
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width:160.0,
+                          height: 160.0,
+                          child: Card(
+
+                            color: Color.white,
+                            elevation: 2.0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0)
+                            ),
+                            child:Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    children: <Widget>[
+                                      // image icon
+                                      SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      Text(
+                                        "Add Help",
+                                        style: TextStyle(
+                                            color: Colors.pink,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20.0
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 5.0,
+                                      ),
+                                    ],
+                                  ),
+                                )
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width:160.0,
+                          height: 160.0,
+                          child: Card(
+
+                            color: Color.white,
+                            elevation: 2.0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0)
+                            ),
+                            child:Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    children: <Widget>[
+                                      // image icons
+                                      SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      Text(
+                                        "Add Vehicle",
+                                        style: TextStyle(
+                                            color: Colors.pink,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20.0
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 5.0,
+                                      ),
+                                    ],
+                                  ),
+                                )
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width:160.0,
+                          height: 160.0,
+                          child: Card(
+
+                            color: Color.white,
+                            elevation: 2.0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0)
+                            ),
+                            child:Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    children: <Widget>[
+                                      // image icons
+                                      SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      Text(
+                                        "Add Guest",
+                                        style: TextStyle(
+                                            color: Colors.pink,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20.0
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 5.0,
+                                      ),
+                                    ],
+                                  ),
+                                )
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width:160.0,
+                          height: 160.0,
+                          child: Card(
+
+                            color: Color.white,
+                            elevation: 2.0,
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8.0)
+                            ),
+                            child:Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Column(
+                                    children: <Widget>[
+                                      // image icon
+                                      SizedBox(
+                                        height: 10.0,
+                                      ),
+                                      Text(
+                                        "Add Entry",
+                                        style: TextStyle(
+                                            color: Colors.pink,
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20.0
+                                        ),
+                                      ),
+                                      SizedBox(
+                                        height: 5.0,
+                                      ),
+                                    ],
+                                  ),
+                                )
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            )
+        )
+    );
+  }
+}
